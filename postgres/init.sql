e74707775e4d5628c217b52a8bda42611a123800
Qi Yang <nayamama@hotmail.com>
2019-08-26 23:04:36 -0400

    modify init.sql

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,9 +1,6 @@
 -- Create a new database called ''
 -- Connect to the 'master' database to run this snippet
 
--- SELECT 'CREATE DATABASE stage_db'
--- WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stage_db');
-
 CREATE DATABASE stage_db;
 create user stage_test with password '1234abcd';
 
