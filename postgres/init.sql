9629e185300ec191b17d1e3096a5520f788d869d
Qi Yang <nayamama@hotmail.com>
2019-08-26 22:39:25 -0400

    add Dockerfile for postgres service

---
+++ postgres/init.sql
@@ -0,0 +1,9 @@
+-- Create a new database called ''
+-- Connect to the 'master' database to run this snippet
+
+SELECT 'CREATE DATABASE stage_db'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'stage_db');
+
+create user stage_test with password '1234abcd';
+
+grant all privileges on database stage_db to stage_test;
